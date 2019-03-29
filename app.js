5531aef07a63fafe63c2116060338b2e6fa12057
jeromelimzw <jerome.lim.zw@gmail.com>
2019-03-29 11:00:39 +0800

    add cors whitelist

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ const helmet = require("helmet");
 const router = require("./routes/books");
 const routerUser = require("./routes/users");
 
+const whitelist = ["https://betterbooks-client.herokuapp.com/"];
+
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use(cors());
 //app.use(morgan("tiny"));
 app.use(helmet());
 
